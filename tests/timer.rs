ab0a20bcc4067d4df1c6425e034c557b47db525f
Drew Crawford <drew@sealedabstract.com>
2021-07-31 19:42:25 -0500

    Timer implementation

---
+++ tests/timer.rs
@@ -0,0 +1,61 @@
+use std::future::Future;
+use std::task::{Waker, Context, Poll};
+use std::pin::Pin;
+use std::time::Duration;
+use std::thread::{spawn, sleep};
+use std::mem::MaybeUninit;
+use std::sync::{Arc, Mutex};
+
+struct SharedState {
+    completed: bool,
+    waker: Option<Waker> //MaybeUnint get_ref was stabilized in 1.55 but not yet available here
+}
+
+///https://rust-lang.github.io/async-book/02_execution/03_wakeups.html
+struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>
+}
+impl Future for TimerFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
+            Poll::Ready(())
+        }
+        else {
+            //could be optimized with will_wake
+            shared_state.waker = Some(cx.waker().to_owned());
+            Poll::Pending
+        }
+    }
+}
+impl TimerFuture {
+    fn new(duration: Duration) -> TimerFuture {
+        //This is kinda bad design.  ideally, we wouldn't do anything until polled, but this is just a test so...
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None
+        }));
+        let move_duration = duration.clone();
+        let cloned_state = shared_state.clone();
+        spawn(move || {
+            let mut shared_state = cloned_state.lock().unwrap();
+            sleep(duration);
+            shared_state.completed = true;
+            shared_state.waker.as_ref().expect("Must wait on this future before completing it").wake_by_ref();
+
+        });
+        TimerFuture {
+            shared_state
+        }
+    }
+}
+
+#[test]
+fn timer() {
+    let mut future = TimerFuture::new(Duration::from_secs(1));
+    while !future.shared_state.lock().unwrap().completed {
+        //spin
+    }
+}
