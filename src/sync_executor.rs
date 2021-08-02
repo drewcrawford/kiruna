29ba217f6f61be7bbc74c9c64c12d0943b8d99f0
Drew Crawford <drew@sealedabstract.com>
2021-08-02 00:45:00 -0500

    Soution based on handles.  This avoids the reference cycle

--- src/sync_executor.rs
+++ src/sync_executor.rs
@@ -1,35 +1,53 @@
 use std::future::{Future};
 use std::sync::mpsc::{Receiver, channel, Sender};
-use crate::sync_task::{SyncTask,ChannelType};
+use crate::sync_task::{SyncTask, SyncWake, ChannelFactory};
+use std::task::{Waker, Context};
+use std::sync::Arc;
 
 pub struct SyncExecutor {
-    channel: Receiver<ChannelType>,
-    spawner: Sender<ChannelType>,
-    num_spawned: u16
+    channel: Receiver<SyncExecutorHandle>,
+    spawner: ChannelFactory,
+    local_spawner: Sender<SyncExecutorHandle>,
+    tasks: Vec<Option<SyncTask>>,
+    num_active: u16,
 }
+
+#[derive(Copy,Clone)]
+pub(crate) struct SyncExecutorHandle(u16);
 
 impl SyncExecutor {
     pub fn new() -> Self {
         let (sender, receiver) = channel();
+        let local_spawner = sender.clone();
+        let factory = unsafe{ ChannelFactory::new(sender)};
         SyncExecutor {
             channel: receiver,
-            spawner: sender,
-            num_spawned: 0
+            spawner: factory,
+            local_spawner,
+            tasks: Vec::new(),
+            num_active: 0
         }
     }
     pub fn spawn(&mut self, future: impl Future<Output=()>+ 'static) {
-        self.num_spawned += 1;
-        let task = SyncTask::new(future, self.spawner.clone());
-        task.begin();
+        let new_handle = SyncExecutorHandle(self.tasks.len() as u16);
+        let task = SyncTask::new(future);
+        self.tasks.push(Some(task));
+        self.num_active += 1;
+        self.local_spawner.send(new_handle).unwrap();
     }
     pub fn drain(mut self) {
         while let Ok(wakeup) = self.channel.recv() {
-            //should be safe because it's only called from this thread
-            let result = unsafe{ wakeup.poll() };
+            let task = self.tasks[wakeup.0 as usize].as_ref().unwrap();
+            let sync_wake = SyncWake::new(wakeup, self.spawner.clone());
+            let as_waker: Waker = Arc::new(sync_wake).into();
+            let mut context = Context::from_waker(&as_waker);
+            //safe because it's always called from this thread
+            let result = unsafe{ task.poll(&mut context) };
             if result.is_ready() {
-                self.num_spawned -= 1;
+                self.num_active -= 1;
+                self.tasks[wakeup.0 as usize].take();
             }
-            if self.num_spawned == 0 { break }
+            if self.num_active == 0 { break }
         }
     }
 }
--- src/sync_task.rs
+++ src/sync_task.rs
@@ -1,55 +1,68 @@
 use std::future::{Future};
 use std::cell::UnsafeCell;
-use std::task::{Wake, Context, Poll, Waker};
+use std::task::{Wake, Context, Poll};
 use std::sync::{Arc};
 use std::pin::Pin;
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{Sender};
+use crate::sync_executor::SyncExecutorHandle;
 
 ///Top-level future
 pub(crate) struct SyncTask {
     future: UnsafeCell<Pin<Box<dyn Future<Output=()>>>>,
-    channel: Sender<ChannelType>,
 }
 
 impl SyncTask {
-    pub(crate) fn new<F: Future<Output=()> + 'static>(future: F, channel: Sender<ChannelType>) -> Self {
+    pub(crate) fn new<F: Future<Output=()> + 'static>(future: F) -> Self {
         SyncTask {
             future: UnsafeCell::new(Box::pin(future
             )),
-            channel: channel
-
         }
     }
-    pub(crate) fn begin(self) {
-        println!("begin task");
-        Arc::new(SyncWake{inner: self}).wake()
+    ///Must call this from the same thread each time
+    pub(crate) unsafe fn poll(&self, cx: &mut Context) -> Poll<()> {
+        let future = &mut *self.future.get();
+        future.as_mut().poll(cx)
     }
 }
 
-pub(crate) struct SyncWake {
-    inner: SyncTask,
+///Ordinarily, channels cannot be shared across threads.  However, we can clone
+/// a channel and use it from one thread.
+///
+/// To use this correctly, you must pass an unused channel into this type.
+#[derive(Clone)]
+pub struct ChannelFactory {
+    inner: Sender<SyncExecutorHandle>
 }
-//We are implementing these even though the type is not really threadsafe.
-//The implication is, any fn that reads or writes to these fields, must be unsafe.
-//Caller must verify that these are never called on multiple threads.
-unsafe impl Send for SyncWake { }
-unsafe impl Sync for SyncWake { }
-pub(crate) type ChannelType = Arc<SyncWake>;
-impl Wake for SyncWake {
-    fn wake(self: Arc<Self>) {
-        //I "believe" this is safe because channel is never mutated.
-        //so this funciton can be called by any thread, meaning it doesn't have to be unsafe.
-        self.inner.channel.send(self.clone()).unwrap();
+
+unsafe impl Send for ChannelFactory {}
+unsafe impl Sync for ChannelFactory {}
+impl ChannelFactory {
+    ///Safety: pass an unused channel in here
+    pub(crate) unsafe fn new(channel: Sender<SyncExecutorHandle>) -> ChannelFactory {
+        ChannelFactory {
+            inner: channel
+        }
     }
+}
+impl ChannelFactory {
+    fn new_channel(&self) -> Sender<SyncExecutorHandle> {
+        self.inner.clone()
+    }
+}
+pub(crate) struct SyncWake {
+    handle: SyncExecutorHandle,
+    channel: ChannelFactory
 }
 impl SyncWake {
-    ///- safety: You must ensure this function is always called from the same thread.
-    pub(crate) unsafe fn poll(self: Arc<Self>) -> Poll<()> {
-        let as_waker: Waker = self.clone().into();
-        let mut context = Context::from_waker(&as_waker);
-        let future_slot = &mut *self.inner.future.get();
-
-        let result = future_slot.as_mut().poll(&mut context);
-        result
+    pub(crate) fn new(handle: SyncExecutorHandle, channel_factory: ChannelFactory) -> SyncWake {
+        SyncWake {
+            handle: handle,
+            channel: channel_factory
+        }
+    }
+}
+impl Wake for SyncWake {
+    fn wake(self: Arc<Self>) {
+        self.channel.new_channel().send(self.handle).unwrap();
     }
 }
