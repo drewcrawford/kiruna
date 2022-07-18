66ca76609a5e18413bcad3492eb9c24842b3b1c0
Drew Crawford <drew@sealedabstract.com>
2022-07-18 13:58:26 -0500

    [test] sparse_await executor

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,7 @@
-use std::task::{Poll, Waker, Context};
-use std::sync::Arc;
+use std::task::{Poll, Waker, Context, Wake};
+use std::sync::{Arc, Mutex};
 use std::future::Future;
+use std::sync::mpsc::{channel, Sender};
 use std::time::{Duration, Instant};
 use super::fake_waker::FakeWaker;
 
@@ -31,4 +32,44 @@ pub fn test_await<F: Future>(future: F, timeout: Duration) -> F::Output {
         }
     }
     panic!("Future never arrived!");
+}
+
+pub struct SimpleWaker(Mutex<Sender<()>>);
+impl Wake for SimpleWaker {
+    fn wake(self: Arc<Self>) {
+        self.0.lock().unwrap().send(()).unwrap();
+    }
+    fn wake_by_ref(self: &Arc<Self>) {
+        self.0.lock().unwrap().send(()).unwrap();
+    }
+}
+
+/**
+Another executor designed for tests.
+
+Instead of polling in a busyloop, this 'executor' polls the future sparsely, only
+when it is required by the API contract.  This behaves more like a real executor, and
+may uncover different bugs.
+*/
+pub fn sparse_await<F: Future>(future: F, timeout: Duration) -> F::Output {
+    let (sender,receiver) = channel();
+    let waker = Arc::new(SimpleWaker(Mutex::new(sender)));
+    let as_waker: Waker = waker.into();
+    let mut as_context = Context::from_waker(&as_waker);
+    let started = Instant::now();
+    let mut pinned = Box::pin(future);
+    loop {
+        if let Poll::Ready(output) = pinned.as_mut().poll(&mut as_context) {
+            return output;
+        }
+        let elapsed = started.elapsed();
+        match timeout.checked_sub(elapsed) {
+            Some(duration) => {
+                receiver.recv_timeout(duration).unwrap();
+            }
+            None => {
+                panic!("Deadline exceeded!");
+            }
+        }
+    }
 }
