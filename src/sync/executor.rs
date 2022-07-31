4c097458663ff076667da6e760ebf327ff70d08c
Drew Crawford <drew@sealedabstract.com>
2022-07-30 23:40:41 -0500

    [sync] Add do_some function

--- src/sync/executor.rs
+++ src/sync/executor.rs
@@ -4,6 +4,7 @@ use std::sync::mpsc::{Receiver, channel, Sender};
 use crate::sync::task::{Task, Wake, ChannelFactory};
 use std::task::{Waker, Context};
 use std::sync::Arc;
+use std::time::Duration;
 
 ///Main executor
 #[derive(Debug)]
@@ -41,25 +42,40 @@ impl<'a> Executor<'a> {
         self.local_spawner.send(new_handle).unwrap();
     }
 
+    /**
+    Runs the executor if there is work to be performed.
+    */
+    pub fn do_some(&mut self) {
+        while let Ok(wakeup) = self.channel.recv_timeout(Duration::ZERO) {
+            self.do_a_wakeup(wakeup)
+        }
+    }
+
+    fn do_a_wakeup(&mut self, wakeup: Handle) {
+        //tasks may send multiple wakeups, theoretically.
+        //if so, don't worry too much about it.
+        if let Some(task) = self.tasks[wakeup.0 as usize].as_ref() {
+            let sync_wake = Wake::new(wakeup, self.spawner.clone());
+            let as_waker: Waker = Arc::new(sync_wake).into();
+            let mut context = Context::from_waker(&as_waker);
+            //safe because it's always called from this thread
+            let result = unsafe{ task.poll(&mut context) };
+            if result.is_ready() {
+                self.num_active -= 1;
+                self.tasks[wakeup.0 as usize].take();
+            }
+        }
+    }
+
     ///Drains the executor.  After this call, the executor can no longer be used.
     ///
     /// This function will return when all spawned tasks complete.
     pub fn drain(mut self) {
+        if self.num_active == 0 { return }
+
         while let Ok(wakeup) = self.channel.recv() {
-            //tasks may send multiple wakeups, theoretically.
-            //if so, don't worry too much about it.
-            if let Some(task) = self.tasks[wakeup.0 as usize].as_ref() {
-                let sync_wake = Wake::new(wakeup, self.spawner.clone());
-                let as_waker: Waker = Arc::new(sync_wake).into();
-                let mut context = Context::from_waker(&as_waker);
-                //safe because it's always called from this thread
-                let result = unsafe{ task.poll(&mut context) };
-                if result.is_ready() {
-                    self.num_active -= 1;
-                    self.tasks[wakeup.0 as usize].take();
-                }
-                if self.num_active == 0 { break }
-            }
+            self.do_a_wakeup(wakeup);
+            if self.num_active == 0 { return }
 
         }
     }
